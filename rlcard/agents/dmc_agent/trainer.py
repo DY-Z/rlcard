508c8469348516bfe79e76de31385bb03a77ac73
DY-Z <52742095+DY-Z@users.noreply.github.com>
2021-12-14 13:34:43 +0800

    Update trainer.py

--- rlcard/agents/dmc_agent/trainer.py
+++ rlcard/agents/dmc_agent/trainer.py
@@ -15,12 +15,10 @@ import traceback
 
 from rlcard.agents.dmc_agent.file_writer import FileWriter
 from rlcard.agents.dmc_agent.model import DMCModel
-from rlcard.agents.dmc_agent.utils import get_batch, create_buffers,  act,  log
-
+from rlcard.agents.dmc_agent.utils import get_batch, create_buffers, create_optimizers, act, log
 
 '''def act(i, device, T, free_queue, full_queue, model, buffers, env):
     try:
-        print("act start")
         log.info('Device %i Actor %i started.', device, i)
         torch_device = torch.device('cuda:'+str(device))
 
@@ -28,50 +26,49 @@ from rlcard.agents.dmc_agent.utils import get_batch, create_buffers,  act,  log
         env.seed(i)
         env.set_agents(model.get_agents())
 
-        done_buf = [[]]
-        episode_return_buf = [[]]
-        target_buf = [[]]
-        state_buf = [[]]
-        action_buf = [[]]
-        size = [0]
+        done_buf = [[] for _ in range(env.num_players)]
+        episode_return_buf = [[] for _ in range(env.num_players)]
+        target_buf = [[] for _ in range(env.num_players)]
+        state_buf = [[] for _ in range(env.num_players)]
+        action_buf = [[] for _ in range(env.num_players)]
+        size = [0 for _ in range(env.num_players)]
 
-        print("loop start")
         while True:
-            print("another iter")
+            print("another iteration")
             trajectories, payoffs = env.run(is_training=True)
-            p = 0
-            size[p] += len(trajectories[p][:-1]) // 2
-            diff = size[p] - len(target_buf[p])
-            if diff > 0:
-              done_buf[p].extend([False for _ in range(diff-1)])
-              done_buf[p].append(True)
-              episode_return_buf[p].extend([0.0 for _ in range(diff-1)])
-              episode_return_buf[p].append(float(payoffs[p]))
-              target_buf[p].extend([float(payoffs[p]) for _ in range(diff)])
-            # State and action
-              for i in range(0, len(trajectories[p])-2, 2):
-                state = trajectories[p][i]['obs']
-                action = env.get_action_feature(trajectories[p][i+1])
-                state_buf[p].append(torch.from_numpy(state))
-                action_buf[p].append(torch.from_numpy(action))
+            for p in range(env.num_players):
+                size[p] += len(trajectories[p][:-1]) // 2
+                diff = size[p] - len(target_buf[p])
+                if diff > 0:
+                    done_buf[p].extend([False for _ in range(diff-1)])
+                    done_buf[p].append(True)
+                    episode_return_buf[p].extend([0.0 for _ in range(diff-1)])
+                    episode_return_buf[p].append(float(payoffs[p]))
+                    target_buf[p].extend([float(payoffs[p]) for _ in range(diff)])
+                    # State and action
+                    for i in range(0, len(trajectories[p])-2, 2):
+                        state = trajectories[p][i]['obs']
+                        action = env.get_action_feature(trajectories[p][i+1])
+                        state_buf[p].append(torch.from_numpy(state))
+                        action_buf[p].append(torch.from_numpy(action))
                 
-            while size[p] > T:
-                index = free_queue[p].get()
-                if index is None:
-                    break
-                for t in range(T):
-                    buffers[p]['done'][index][t, ...] = done_buf[p][t]
-                    buffers[p]['episode_return'][index][t, ...] = episode_return_buf[p][t]
-                    buffers[p]['target'][index][t, ...] = target_buf[p][t]
-                    buffers[p]['state'][index][t, ...] = state_buf[p][t]
-                    buffers[p]['action'][index][t, ...] = action_buf[p][t]
-                full_queue[p].put(index)
-                done_buf[p] = done_buf[p][T:]
-                episode_return_buf[p] = episode_return_buf[p][T:]
-                target_buf[p] = target_buf[p][T:]
-                state_buf[p] = state_buf[p][T:]
-                action_buf[p] = action_buf[p][T:]
-                size[p] -= T
+                while size[p] > T:
+                    index = free_queue[p].get()
+                    if index is None:
+                        break
+                    for t in range(T):
+                        buffers[p]['done'][index][t, ...] = done_buf[p][t]
+                        buffers[p]['episode_return'][index][t, ...] = episode_return_buf[p][t]
+                        buffers[p]['target'][index][t, ...] = target_buf[p][t]
+                        buffers[p]['state'][index][t, ...] = state_buf[p][t]
+                        buffers[p]['action'][index][t, ...] = action_buf[p][t]
+                    full_queue[p].put(index)
+                    done_buf[p] = done_buf[p][T:]
+                    episode_return_buf[p] = episode_return_buf[p][T:]
+                    target_buf[p] = target_buf[p][T:]
+                    state_buf[p] = state_buf[p][T:]
+                    action_buf[p] = action_buf[p][T:]
+                    size[p] -= T
 
     except KeyboardInterrupt:
         pass
@@ -79,24 +76,7 @@ from rlcard.agents.dmc_agent.utils import get_batch, create_buffers,  act,  log
         log.error('Exception in worker process %i', i)
         traceback.print_exc()
         print()
-        raise e
-'''
-
-def create_optimizers(trainable_posi, learning_rate, momentum, epsilon, alpha, learner_model):
-    optimizers = []
-    for player_id in range(len(trainable_posi)):
-      if trainable_posi[player_id] == 1:
-        optimizer = torch.optim.RMSprop(
-            learner_model.parameters(player_id),
-            lr=learning_rate,
-            momentum=momentum,
-            eps=epsilon,
-            alpha=alpha)
-      else:
-        optimizer = 0
-
-      optimizers.append(optimizer)
-    return optimizers
+        raise e'''
 
 def compute_loss(logits, targets):
     loss = ((logits - targets)**2).mean()
@@ -119,8 +99,6 @@ def learn(position,
     episode_returns = batch['episode_return'][batch['done']]
     mean_episode_return_buf[position].append(torch.mean(episode_returns).to(device))
 
-    print("Called")
-
     with lock:
         values = agent.forward(state, action)
         loss = compute_loss(values, target)
@@ -142,12 +120,12 @@ def learn(position,
 class DMCTrainer:    
     def __init__(self,
                  env,
-                 trainable_posi = [1,0,0,0],
+                 trainable = [1,0,0,0],
                  load_model=False,
                  xpid='dmc',
                  save_interval=30,
                  num_actor_devices=1,
-                 num_actors = 1,
+                 num_actors = 5,
                  training_device=0,
                  savedir='experiments/dmc_result',
                  total_frames=100000000000,
@@ -155,7 +133,7 @@ class DMCTrainer:
                  batch_size=32,
                  unroll_length=100,
                  num_buffers=50,
-                 num_threads=1,
+                 num_threads=4,
                  max_grad_norm=40,
                  learning_rate=0.001,
                  alpha=0.99,
@@ -163,7 +141,6 @@ class DMCTrainer:
                  epsilon=0.00001):
         '''
         Deep Monte-Carlo
-
         Args:
             env: RLCard environment
             load_model (boolean): Whether loading an existing model
@@ -215,31 +192,24 @@ class DMCTrainer:
         self.momentum = momentum
         self.epsilon = epsilon
 
-        self.trainable_posi = trainable_posi
-        self.total_trainable = sum(trainable_posi)
+        self.trainable = trainable 
 
         self.action_shape = self.env.action_shape
         if self.action_shape[0] == None:  # One-hot encoding
             self.action_shape = [[self.env.num_actions] for _ in range(self.env.num_players)]
 
         self.mean_episode_return_buf = [deque(maxlen=100) for _ in range(self.env.num_players)]
-        '''self.mean__episode_return_buf = []
-        for p in range(len(trainable_posi)):
-          if trainable_posi[p] == 1:
-            self.mean__episode_return_buf.append(deque(maxlen=100))
-          else:
-            self.mean__episode_return_buf.append([])'''
 
     def start(self):
         # Initialize actor models
         models = []
         for device in range(self.num_actor_devices):
-            model = DMCModel(self.trainable_posi,
-                      self.env.state_shape,
-                      self.action_shape,
-                      exp_epsilon=self.exp_epsilon,
-                      device=device)
-            
+            model = DMCModel(
+                             self.env.state_shape,
+                             self.action_shape,
+                             self.trainable,
+                             exp_epsilon=self.exp_epsilon,
+                             device=device)
             model.share_memory()
             model.eval()
             models.append(model)
@@ -255,24 +225,21 @@ class DMCTrainer:
         ctx = mp.get_context('spawn')
         free_queue = []
         full_queue = []
-        print("reach full queue")
         for device in range(self.num_actor_devices):
             _free_queue = [ctx.SimpleQueue() for _ in range(self.env.num_players)]
             _full_queue = [ctx.SimpleQueue() for _ in range(self.env.num_players)]
             free_queue.append(_free_queue)
             full_queue.append(_full_queue)
 
-        print("finished full queue")
         # Learner model for training
-        learner_model = DMCModel(self.trainable_posi,
-                      self.env.state_shape,
-                      self.action_shape,
-                      device=self.training_device)
-        
-        print(learner_model.agents)
+        learner_model = DMCModel(
+                                  self.env.state_shape,
+                                 self.action_shape,
+                                 self.trainable,
+                                 device=self.training_device)
 
         # Create optimizers
-        optimizers = create_optimizers(self.trainable_posi,
+        optimizers = create_optimizers(self.env.num_players,
                                        self.learning_rate,
                                        self.momentum,
                                        self.epsilon,
@@ -282,7 +249,6 @@ class DMCTrainer:
         # Stat Keys
         stat_keys = []
         for p in range(self.env.num_players):
-          if self.trainable_posi[p] == 1:
             stat_keys.append('mean_episode_return_'+str(p))
             stat_keys.append('loss_'+str(p))
         frames, stats = 0, {k: 0 for k in stat_keys}
@@ -293,7 +259,6 @@ class DMCTrainer:
                     self.checkpointpath, map_location="cuda:"+str(self.training_device)
             )
             for p in range(self.env.num_players):
-              if self.trainable_posi[p] == 1:
                 learner_model.get_agent(p).load_state_dict(checkpoint_states["model_state_dict"][p])
                 optimizers[p].load_state_dict(checkpoint_states["optimizer_state_dict"][p])
                 for device in range(self.num_actor_devices):
@@ -302,23 +267,17 @@ class DMCTrainer:
             frames = checkpoint_states["frames"]
             log.info(f"Resuming preempted job, current stats:\n{stats}")
 
-        print("done load model")
+
         # Starting actor processes
         for device in range(self.num_actor_devices):
             num_actors = self.num_actors
             for i in range(self.num_actors):
-                print(i, "reach")
                 actor = ctx.Process(
                     target=act,
                     args=(i, device, self.T, free_queue[device], full_queue[device], models[device], buffers[device], self.env))
-                print("actor done")
-                print(actor)
                 actor.start()
-                print("finish start")
                 actor_processes.append(actor)
-                print(i, "done")
 
-        print("done actor process")
         def batch_and_learn(i, device, position, local_lock, position_lock, lock=threading.Lock()):
             """Thread target for the learning process."""
             nonlocal frames, stats
@@ -338,19 +297,15 @@ class DMCTrainer:
         for device in range(self.num_actor_devices):
             for m in range(self.num_buffers):
                 for p in range(self.env.num_players):
-                  if self.trainable_posi[p] == 1:
                     free_queue[device][p].put(m)
 
-        threads = [] 
-        locks = [[threading.Lock() if self.trainable_posi[i] == 1 else 0 for i in range(self.env.num_players)] for _ in range(self.num_actor_devices)]
+        threads = []
+        locks = [[threading.Lock() for _ in range(self.env.num_players)] for _ in range(self.num_actor_devices)]
+        position_locks = [threading.Lock() for _ in range(self.env.num_players)]
 
-        #position_locks = [threading.Lock() for _ in range(self.env.num_players)]
-        position_locks = [threading.Lock() if self.trainable_posi[i] == 1 else 0 for i in range(self.env.num_players)]
-        
         for device in range(self.num_actor_devices):
             for i in range(self.num_threads):
                 for position in range(self.env.num_players):
-                  if self.trainable_posi[position] == 1:
                     thread = threading.Thread(
                         target=batch_and_learn, name='batch-and-learn-%d' % i, args=(i,device,position,locks[device][position],position_locks[position]))
                     thread.start()
@@ -360,15 +315,14 @@ class DMCTrainer:
             log.info('Saving checkpoint to %s', self.checkpointpath)
             _agents = learner_model.get_agents()
             torch.save({
-                'model_state_dict': [_agents[i].state_dict() if self.trainable_posi[i] == 1 else 0 for i in range(len(_agents))],
-                'optimizer_state_dict': [optimizers[i].state_dict() if self.trainable_posi[i] == 1 else 0 for i in range(len(optimizers))],
+                'model_state_dict': [_agent.state_dict() for _agent in _agents],
+                'optimizer_state_dict': [optimizer.state_dict() for optimizer in optimizers],
                 "stats": stats,
                 'frames': frames,
             }, self.checkpointpath)
 
             # Save the weights for evaluation purpose
             for position in range(self.env.num_players):
-              if self.trainable_posi[position] == 1:
                 model_weights_dir = os.path.expandvars(os.path.expanduser(
                     '%s/%s/%s' % (self.savedir, self.xpid, str(position)+'_'+str(frames)+'.pth')))
                 torch.save(learner_model.get_agent(position), model_weights_dir)
